e5b40d250b5b481b3b210cdb947b1454b0587892
mikietechie <mzinyoni7@outlook.com>
2021-08-11 12:31:09 +0200

    ....., done for now

--- mediplus_backend/app/admin_site.py
+++ mediplus_backend/app/admin_site.py
@@ -3,7 +3,7 @@ import os
 
 
 admin_site = site
-admin_site.login_template = os.path.join('app', 'common', 'login.html')
-admin_site.site_title = "Aone Site Admin"
-admin_site.site_header = "Aone clients website adminstration"
-admin_site.index_title = "Website content site adminstration"
+#admin_site.login_template = os.path.join('app', 'login.html')
+admin_site.site_title = "Mediplus Admin"
+admin_site.site_header = "Mediplus Content Admin"
+admin_site.index_title = "Mediplus Admin | Home"
